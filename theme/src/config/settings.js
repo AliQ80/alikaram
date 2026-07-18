5f481380a3b848e6cc29542ea280491e91629425
Ali Karam <ak@alikaram.com>
2026-07-18 11:06:51 +0300

    upgrade to 3.1

--- theme/src/config/settings.js
+++
@@ -1,8 +0,0 @@
-export default {
-	title: `Ali Karam Website`,
-	description: `Web Developer & Computer Teacher`,
-	url: `https://alikaram.com`, // No trailing slash!
-	name: `AliKaram`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
-};
