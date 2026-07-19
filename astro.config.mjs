a3a7ee6bdfe222ae46ddc3ea2c7a87cd2a320eed
Ali Karam <ak@alikaram.com>
2026-07-20 00:48:38 +0300

    fix: set canonical site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://alikaram.com/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   integrations: [sitemap(), mdx(), icon()], // Add renderers to the config
 });
