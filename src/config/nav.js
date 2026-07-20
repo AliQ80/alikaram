f42d2be8e62644f05bac84ef286e0efd353e3b0e
Ali Karam <ak@alikaram.com>
2026-07-20 19:10:18 +0300

    feat: add icons to nav links, unify Projects/Blog icons with hero buttons

--- src/config/nav.js
+++ src/config/nav.js
@@ -2,21 +2,26 @@ export const nav = [
 	{
 		title: 'Home',
 		slug: '/',
+		icon: 'mdi:home-outline',
 	},
 	{
 		title: 'Projects',
 		slug: '/projects',
+		icon: 'mdi:briefcase-outline',
 	},
 	{
 		title: 'Blog',
 		slug: '/blog',
+		icon: 'mdi:notebook-outline',
 	},
 	{
 		title: 'About',
 		slug: '/info/about',
+		icon: 'mdi:account-outline',
 	},
 	{
 		title: 'Contact',
 		slug: '/info/contact',
+		icon: 'mdi:email-outline',
 	},
 ];
